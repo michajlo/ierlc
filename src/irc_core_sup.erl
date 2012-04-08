7af0bc60fff434f07bb3688daa147a5f69db3bcd
Michajlo Matijkiw <michajlo.matijkiw@gmail.com>
2012-04-08 00:29:50 -0400

    Wrap up irc_event_manager and irc_session into a supervisor

---
+++ src/irc_core_sup.erl
@@ -0,0 +1,16 @@
+-module(irc_core_sup).
+
+-behaviour(supervisor).
+
+-export([start_link/3]).
+
+-export([init/1]).
+
+start_link(Host, Port, Id) ->
+    supervisor:start_link(?MODULE, [Host, Port, Id]).
+
+init([Host, Port, Id]) -> 
+    {ok, {{one_for_one, 5, 30000},
+      [{event_manager, {irc_event_manager, start_link, []}, permanent, 3000, worker, [irc_event_manager]},
+      {irc_session, {irc_session, start_link, [Host, Port, Id]}, permanent, 3000, worker, [irc_session]}]}}.
+      
