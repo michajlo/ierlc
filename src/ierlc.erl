f4fc5663a6c8b6e5749d797dd5a776af529b78e2
Michajlo Matijkiw <michajlo.matijkiw@gmail.com>
2012-04-08 01:15:58 -0400

    Make startup a little more convenient

--- src/ierlc.erl
+++ src/ierlc.erl
@@ -1,10 +1,15 @@
 -module(ierlc).
 
+-export([start_app/0]).
+
 -export([start/2]).
+
+start_app() ->
+    application:start(sasl),
+    application:start(ierlc).
 
 start(_Type, _Args) ->
     {ok, Host} = application:get_env(host),
     {ok, Port} = application:get_env(port),
     {ok, Id} = application:get_env(id),
-    application:start(sasl),
     irc_core_sup:start_link(Host, Port, Id).
