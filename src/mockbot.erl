7728bbbaa6aaf6b01bf98177ccfa529610b2d7f0
Michajlo Matijkiw <michajlo.matijkiw@gmail.com>
2012-04-08 00:28:20 -0400

    irc -> irc_session, also take anything in init

--- src/mockbot.erl
+++ src/mockbot.erl
@@ -2,14 +2,14 @@
 
 -export([init/1, handle_event/2, terminate/2]).
 
-init([]) ->
+init(_) ->
     {ok, nil}.
 
 handle_event({_, privmsg, ["#" ++ Room, Msg]}, State) ->
-    irc:msg(["#"|Room], Msg),
+    irc_session:msg(["#"|Room], Msg),
     {ok, State};
 handle_event({{Nick, _}, privmsg, [_, Msg]}, State) ->
-    irc:msg(Nick, Msg),
+    irc_session:msg(Nick, Msg),
     {ok, State};
 handle_event(Any, State) ->
     io:format("Unhandled: ~p~n", [Any]),
