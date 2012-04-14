76e955a0f12fedd3f281c7e25d7454b9ff5a8a00
Michajlo Matijkiw <michajlo.matijkiw@gmail.com>
2012-04-14 19:48:59 -0400

    some convenience functions

--- src/ierlc.erl
+++ src/ierlc.erl
@@ -1,6 +1,6 @@
 -module(ierlc).
 
--export([start/0]).
+-export([start/0, join/1, msg/2]).
 
 -export([start/2]).
 
@@ -12,3 +12,7 @@ start(_Type, _Args) ->
     {ok, Port} = application:get_env(port),
     {ok, Id} = application:get_env(id),
     irc_core_sup:start_link(Host, Port, Id).
+
+join(Room) -> irc_session:join(Room).
+
+msg(To, Msg) -> irc_session:msg(To, Msg).
