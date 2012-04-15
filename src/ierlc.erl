caa9bf35f90153e17f10a8da65641f642890d4fb
Michajlo Matijkiw <michajlo.matijkiw@gmail.com>
2012-04-15 14:39:05 -0400

    break out ierlc_app

--- src/ierlc.erl
+++ src/ierlc.erl
@@ -1,17 +1,10 @@
 -module(ierlc).
 
--export([start/0, join/1, msg/2]).
+-export([start/0, stop/0, join/1, msg/2]).
 
--export([start/2]).
+start() -> application:start(ierlc).
 
-start() ->
-    application:start(ierlc).
-
-start(_Type, _Args) ->
-    {ok, Host} = application:get_env(host),
-    {ok, Port} = application:get_env(port),
-    {ok, Id} = application:get_env(id),
-    irc_core_sup:start_link(Host, Port, Id).
+stop() -> application:stop(ierlc).
 
 join(Room) -> irc_session:join(Room).
 
---
+++ src/ierlc_app.erl
@@ -0,0 +1,13 @@
+-module(ierlc_app).
+
+-behaviour(application).
+
+-export([start/2, stop/1]).
+
+start(_Type, _Args) ->
+    {ok, Host} = application:get_env(host),
+    {ok, Port} = application:get_env(port),
+    {ok, Id} = application:get_env(id),
+    irc_core_sup:start_link(Host, Port, Id).
+
+stop(_State) -> ok.
