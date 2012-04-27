d0209fa4f1afdf442f963684ce35f59c63b12220
Michajlo Matijkiw <michajlo.matijkiw@gmail.com>
2012-04-26 23:00:51 -0400

    convenience functions

--- src/ierlc.erl
+++ src/ierlc.erl
@@ -1,6 +1,10 @@
 -module(ierlc).
 
--export([start/0, stop/0, join/1, msg/2]).
+-export([start/0, stop/0]).
+
+-export([join/1, msg/2]).
+
+-export([add_handler/2, delete_handler/2]).
 
 start() -> application:start(ierlc).
 
@@ -9,3 +13,8 @@ stop() -> application:stop(ierlc).
 join(Room) -> irc_session:join(Room).
 
 msg(To, Msg) -> irc_session:msg(To, Msg).
+
+add_handler(Handler, Args) -> irc_event_manager:add_handler(Handler, Args).
+
+delete_handler(Handler, Args) -> irc_event_manager:delete_handler(Handler, Args).
+
