de7fc56eacf1abcad172bcd21f97b50375eb1517
Michajlo Matijkiw <michajlo.matijkiw@gmail.com>
2012-04-26 22:51:41 -0400

    event manager's children dynamic

--- src/irc_core_sup.erl
+++ src/irc_core_sup.erl
@@ -11,6 +11,6 @@ start_link(Host, Port, Id) ->
 
 init([Host, Port, Id]) -> 
     {ok, {{one_for_one, 5, 30000},
-      [{event_manager, {irc_event_manager, start_link, []}, permanent, 3000, worker, [irc_event_manager]},
+      [{event_manager, {irc_event_manager, start_link, []}, permanent, 3000, worker, dynamic},
       {irc_session, {irc_session, start_link, [Host, Port, Id]}, permanent, 3000, worker, [irc_session]}]}}.
       
